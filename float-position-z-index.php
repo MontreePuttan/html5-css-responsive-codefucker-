19241aa8f754b7ccafb37e6baf7b16d8326eb275
MONTREE\mohara2 <scoopy_825@hotmail.com>
2016-11-22 10:02:29 +0700

    การกำหนด float position และ z-index

---
+++ float-position-z-index.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>title</title>
+        <style>
+            .klong{
+               width: 150px;
+               height: 150px;
+               border-style: solid;
+               border-width: 1px;
+               float: left;
+               
+            }
+            #red{
+                background: red;
+            }
+            #yellow{
+                background: yellow;
+            }
+            #green{
+                background: green;
+            }
+            .box{
+                width: 50px;
+                height: 50px;
+                border-style: solid;
+                border-width: 1px;
+                background: blue;
+                float: inherit;
+                margin: 15px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="klong">
+            
+        </div>
+        <div class="klong" id="red">
+            
+        </div>
+        <div class="klong" id="yellow">
+            <div class="box">
+                
+            </div>
+        </div>
+        <div class="klong" id="green">
+            
+        </div>
+        <div class="klong">
+            
+        </div>
+    </body>
+</html>
