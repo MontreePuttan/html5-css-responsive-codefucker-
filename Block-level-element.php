065775bb916ebf00f6d464ce6385085cd55b98fa
MONTREE\mohara2 <scoopy_825@hotmail.com>
2016-11-22 09:36:44 +0700

    สีสัน และขอบเขตของ Block-level element

---
+++ Block-level-element.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>title</title>
+        <style>
+            .krapong{
+                border-style: solid;
+                border-width: 1px;
+                background: #35B3EF;
+                width: 80%;
+                height: 200px;
+                overflow: auto;
+                padding: 30px;/**/
+                border-width: 30px;
+                border-color: green;
+            }
+            body{
+                background: #257CA2;
+            }
+            #piset{
+                background: #29DB57;
+                margin: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="krapong">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </div>
+        <div class="krapong" id="piset">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </div>
+        <div class="krapong">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </div>
+    </body>
+</html>
+
+
