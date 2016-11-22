b3928989a8009b02c0e9a67f78aa5ef16b3fbc97
MONTREE\mohara2 <scoopy_825@hotmail.com>
2016-11-22 14:16:30 +0700

    media query ทำเว็บ responsive web design ตอน 1

---
+++ media.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            span{
+                color: red;
+            }
+            .klong{
+
+                float: left;
+                border: solid 1px red;
+                box-shadow: 5px 5px rgba(255,0,0,0.5);
+                margin: 10px;
+                padding: 5px;
+                box-sizing: border-box;
+            }
+
+            @media (min-width:470px){
+                .thad{
+                    width: 50%;
+                    float: left;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="thad">
+            <div class="klong">
+                <span>paragraph 1</span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </div>
+        </div>
+        <div class="thad">
+            <div class="klong">
+                <span>paragraph 2</span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </div>
+        </div>
+    </body>
+</html>
