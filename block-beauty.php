2a1712ec0db26791f8af00e6e6a8aa50ac1c9f44
MONTREE\mohara2 <scoopy_825@hotmail.com>
2016-11-22 12:17:38 +0700

    ตกแต่ง สร้างปุ่ม เริ่มสร้างหน้าเว็บสำหรับมือถือ

---
+++ block-beauty.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            .klong{
+                border: solid 2px red;
+                width: 60%;
+                margin: auto;
+                margin-top: 100px;
+                padding: 25px;
+                border-radius: 5px;
+                box-shadow: 10px 10px rgba(255,170,170,0.5);
+            } 
+            .btn-happy{
+                border: solid 2px pink;
+                margin: auto;
+                padding: 5px;
+                text-align: center;
+                border-radius: 5px;
+            }
+            .btn-happy:hover{
+                box-shadow: 6px 6px gray;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="klong">
+            <h3>Header</h3>
+            <p>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </p>
+            <div class="btn-happy">
+                Happy
+            </div>
+        </div>
+
+    </body>
+</html>
